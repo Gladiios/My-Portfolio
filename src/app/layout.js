7bba89871f21c9347d24a26914ed5b4e8050338b
erwan <erwan-14730@outlook.fr>
2024-01-28 04:02:45 +0100

    reset as how it was 7d ago

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,25 +2,13 @@ import Head from "next/head";
 import Providers from "./Providers";
 
 export default function RootLayout({ children }) {
-  const jsonLdData = {
-    "@context": "http://schema.org",
-    name: "Erwan Marie - Web Developer",
-    description:
-      "Je suis un développeur web spécialisé Front-End avec une expertise en React. Découvrez mon portfolio et les différents projets sur lesquels j'ai travaillé.",
-    address: {
-      "@type": "PostalAddress",
-      addressLocality: "Caen",
-      postalCode: "14000",
-    },
-    url: "https://www.erwan-marie.dev/",
-  };
   return (
     <div>
       <Head>
         <title>Erwan Marie - Web Developer</title>
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLdData) }}
+        <meta
+          name="description"
+          content="Je suis un développeur web spécialisé Front-End,avec une expertise en React.Découvrez mon portfolio et les différents projets sur lesquels j'ai travaillé."
         />
       </Head>
       <Providers>{children}</Providers>
