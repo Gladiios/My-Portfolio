8adef4dc2addee2dd0954a9f4903a051d30fa90e
erwan <erwan-14730@outlook.fr>
2024-01-28 03:47:13 +0100

    another modification for the seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,25 +1,28 @@
 import Head from "next/head";
 import Providers from "./Providers";
+import Script from "next/script";
 
 export default function RootLayout({ children }) {
+  const jsonLdData = {
+    "@context": "http://schema.org",
+    name: "Erwan Marie - Web Developer",
+    description:
+      "Je suis un développeur web spécialisé Front-End, avec une expertise en React. Découvrez mon portfolio et les différents projets sur lesquels j'ai travaillé.",
+    address: {
+      "@type": "PostalAddress",
+      addressLocality: "Caen",
+      postalCode: "14000",
+    },
+    url: "https://www.erwan-marie.dev/",
+  };
   return (
     <div>
       <Head>
         <title>Erwan Marie - Web Developer</title>
-        <script type="application/ld+json">
-          {JSON.stringify({
-            "@context": "http://schema.org",
-            name: "Erwan Marie - Web Developer",
-            description:
-              "Je suis un développeur web spécialisé Front-End, avec une expertise en React. Découvrez mon portfolio et les différents projets sur lesquels j'ai travaillé.",
-            address: {
-              "@type": "PostalAddress",
-              addressLocality: "Caen",
-              postalCode: "14000",
-            },
-            url: "https://www.erwan-marie.dev/",
-          })}
-        </script>
+        <Script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLdData) }}
+        />
       </Head>
       <Providers>{children}</Providers>
     </div>
