5de2eef9df074491e61137975b94e759286e5794
erwan <erwan-14730@outlook.fr>
2024-03-12 16:48:17 +0100

    Added vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Providers from "./Providers";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({ children }) {
   return (
@@ -11,7 +12,10 @@ export default function RootLayout({ children }) {
           content="Je suis un développeur web spécialisé Front-End,avec une expertise en React.Découvrez mon portfolio et les différents projets sur lesquels j'ai travaillé."
         />
       </Head>
-      <Providers>{children}</Providers>
+      <Providers>
+        {children}
+        <Analytics />
+      </Providers>
     </div>
   );
 }
